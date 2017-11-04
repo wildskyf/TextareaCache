b648fed3790f3d8ce0c40c84f45b781c5d94388c
wildskyf <poppin.wildsky@gmail.com>
2017-11-04 23:40:11 +0800

    refactor panel.js

--- panel.js
+++ panel.js
@@ -1,173 +1,186 @@
 // popup script
 
-var setBodyEmpty = () => {
-    var textnode = document.createTextNode('There is no cache here!');
-    document.body.textContent = '';
-    document.body.style.padding = '20px';
-    document.body.appendChild(textnode);
-};
-
-var formatDate = key => {
-    var timestamp = key.split(' ')[0];
-    var tmp_key_array = key.split(' ').reverse();
-    tmp_key_array.pop();
-    var else_info = tmp_key_array.reverse().join(' ');
-    if (!timestamp.includes('/')) {
-        var origin_date = new Date(Number(timestamp));
-        var year = origin_date.getFullYear();
-        var month = origin_date.getMonth()+1;
-        var day = origin_date.getDate();
-        timestamp = `${year}/${month}/${day}`;
-    }
-    return timestamp + ' ' + else_info;
-};
-
 var panel = {
+
+    $select        : null,
+    $show_cache    : null,
+    $copy_btn      : null,
+    $delete_btn    : null,
+    $delete_all_btn: null,
+
+    setBodyEmpty: () => {
+        var textnode = document.createTextNode('There is no cache here!');
+        document.body.textContent = '';
+        document.body.style.padding = '20px';
+        document.body.appendChild(textnode);
+    },
+
+    _formatDate: key => {
+        var timestamp = key.split(' ')[0];
+        var tmp_key_array = key.split(' ').reverse();
+        tmp_key_array.pop();
+        var else_info = tmp_key_array.reverse().join(' ');
+        if (!timestamp.includes('/')) {
+            var origin_date = new Date(Number(timestamp));
+            var year = origin_date.getFullYear();
+            var month = origin_date.getMonth()+1;
+            var day = origin_date.getDate();
+            timestamp = `${year}/${month}/${day}`;
+        }
+        return timestamp + ' ' + else_info;
+    },
+
+    showPreview: (isWYSIWYG, val) => {
+        // val is used to show preview of WYSIWYG,
+        // so it should not be escaped.
+        //
+        // for security issues, I will remove all <script> & </script> tag
+
+        if (isWYSIWYG) {
+            $show_cache.type = 'WYSIWYG';
+            val = val.replace(/<script.*>.*<\/script.*>/g, '');
+            $show_cache.innerHTML = val;
+        }
+        else {
+            $show_cache.type = 'txt';
+            var text = document.createTextNode(val);
+            var textarea = document.createElement('textarea');
+            textarea.appendChild(text);
+            $show_cache.innerHTML = '';
+            $show_cache.appendChild(textarea);
+        }
+    },
+
+    selectText: dom => {
+        if (document.selection) {
+            var range = document.body.createTextRange();
+            range.moveToElementText(dom);
+            range.select();
+        }
+        else if (window.getSelection) {
+            var range = document.createRange();
+            range.selectNode(dom);
+            window.getSelection().addRange(range);
+        }
+    },
+
+    _reverse: obj_array => {
+        var tmp_array = []; // for reverse the order of saved cache
+        for (var key in obj_array) {
+            if (!obj_array[key]) continue;
+            var tmp_data = obj_array[key];
+            tmp_data.key = key;
+            tmp_array.push(tmp_data);
+        }
+
+        return tmp_array.reverse();
+    },
+
+    showSelect: (whole_data) => {
+        var me = panel;
+        var { $select, $show_cache } = me;
+
+        me._reverse(whole_data).forEach(one_data => {
+            var { type } = one_data;
+            var cache = one_data.val;
+            var txt = me._formatDate(one_data.key).split(" ");
+            txt.pop(); // hide the serial number
+            txt = txt.join(' ');
+
+            var option = document.createElement('option');
+            option.textContent = txt; // todo: remove date stamp
+            option.value = one_data.key;
+            option.title = `${me._formatDate(one_data.key)}\n(${one_data.url})\n\n${one_data.val.substr(0,40)}...`;
+
+            $select.appendChild(option);
+
+            if ($show_cache.innerHTML == '')
+                me.showPreview(type == 'WYSIWYG', cache);
+
+            if (!document.querySelector('option')) {
+                me.setBodyEmpty();
+            }
+        });
+    },
+
     init: () => {
+        var me = panel;
         var whole_data = null;
+
         browser.runtime.sendMessage({
             behavior: 'load'
         }).then( ( resObj => {
+
             if (!(resObj && resObj.data)) return false;
+
+            delete resObj.data.version;
+            delete resObj.data.setting;
+
             whole_data = resObj.data;
-            if (Object.keys(whole_data).length <= 2) {
-                // Note: there are 2 default keys: version, setting
-                setBodyEmpty();
+            if (Object.keys(whole_data).length <= 0) {
+                me.setBodyEmpty();
                 return false;
             }
 
-            var selector   = document.querySelector('#cache_seletor');
-            var show_cache = document.querySelector('#show_cache');
-            var copy_btn   = document.querySelector('#copy_btn');
-            var delete_btn = document.querySelector('#delete_btn');
+            $select         = me.$select         = document.querySelector('#cache_seletor');
+            $show_cache     = me.$show_cache     = document.querySelector('#show_cache');
+            $copy_btn       = me.$copy_btn       = document.querySelector('#copy_btn');
+            $delete_btn     = me.$delete_btn     = document.querySelector('#delete_btn');
+            $delete_all_btn = me.$delete_all_btn = document.querySelector('#delete_all_btn');
 
-            var escapeHTML = str => str.toString().replace(/[&"'<>]/g, m => ({
-                "&": "&amp;",
-                '"': "&quot;",
-                "'": "&#39;",
-                "<": "&lt;",
-                ">": "&gt;"
-            })[m]);
+            me.showSelect(whole_data);
 
-            var selectText = dom  => {
-                if (document.selection) {
-                    var range = document.body.createTextRange();
-                    range.moveToElementText(dom);
-                    range.select();
-                }
-                else if (window.getSelection) {
-                    var range = document.createRange();
-                    range.selectNode(dom);
-                    window.getSelection().addRange(range);
-                }
-            };
-
-            var showPreview = (isWYSIWYG, val) => {
-                // val is used to show preview of WYSIWYG,
-                // so it should not be escaped.
-                //
-                // for security issues, I will remove all <script> & </script> tag
-
-                if (isWYSIWYG) {
-                    show_cache.type = 'WYSIWYG';
-                    val = val.replace(/<script.*>.*<\/script.*>/g, '');
-                    show_cache.innerHTML = val;
-                }
-                else {
-                    show_cache.type = 'txt';
-                    var text = document.createTextNode(val);
-                    var textarea = document.createElement('textarea');
-                    textarea.appendChild(text);
-                    show_cache.innerHTML = '';
-                    show_cache.appendChild(textarea);
-                }
-            };
-
-
-            var tmp_array = []; // for reverse the order of saved cache
-            for (var key in whole_data) {
-                if (!whole_data[key] || key == 'version' || key == 'setting') continue;
-                var tmp_data = whole_data[key];
-                tmp_data.key = key;
-                tmp_array.push(tmp_data);
-            }
-
-            tmp_array.reverse().forEach(one_data => {
-                var type = one_data.type;
-                var cache = one_data.val;
-                var txt = formatDate(one_data.key).split(" ");
-                txt.pop(); // hide the serial number
-                txt = txt.join(' ');
-
-                var option = document.createElement('option');
-                option.textContent = txt; // todo: remove date stamp
-                option.value = one_data.key;
-                option.title = `${formatDate(one_data.key)}\n(${one_data.url})\n\n${one_data.val.substr(0,40)}...`;
-
-                selector.appendChild(option);
-
-                if (show_cache.innerHTML == '')
-                    showPreview(type == 'WYSIWYG', cache);
-
-                if (!document.querySelector('option')) {
-                    setBodyEmpty();
-                }
-            });
-
-            selector.addEventListener('change', e => {
+            $select.addEventListener('change', e => {
                 var key = e.target.value;
                 var cache = whole_data[key].val;
                 var isWYSIWYG = whole_data[key].type == 'WYSIWYG';
 
-                showPreview(isWYSIWYG, cache);
+                me.showPreview(isWYSIWYG, cache);
             });
 
-            copy_btn.addEventListener('click', () => {
-                if (show_cache.type == 'WYSIWYG') {
-                    selectText(show_cache);
+            $copy_btn.addEventListener('click', () => {
+                if ($show_cache.type == 'WYSIWYG') {
+                    me.selectText($show_cache);
                 }
                 else {
                     document.querySelector("textarea").select();
                 }
                 document.execCommand("Copy");
-                // alert('You got it, now put your cache anyway!');
             });
 
-
-            delete_all_btn.addEventListener('click', () => {
+            $delete_all_btn.addEventListener('click', () => {
                 browser.runtime.sendMessage({
                     behavior: 'clear'
                 }).then(res => {
                     if (res.msg == 'done') {
-                        setBodyEmpty();
+                        me.setBodyEmpty();
                     }
                 });
             });
 
-            delete_btn.addEventListener('click', () => {
+            $delete_btn.addEventListener('click', () => {
                 browser.runtime.sendMessage({
                     behavior: 'delete',
-                    id: selector.value
+                    id: $select.value
                 }).then( res => {
                     whole_data = res.data;
-                    selector.querySelector(`[value="${res.deleted}"]`).remove();
+                    $select.querySelector(`[value="${res.deleted}"]`).remove();
 
-                    var key = selector.value;
+                    var key = $select.value;
 
                     if (!whole_data[key]) {
-                        setBodyEmpty();
+                        me.setBodyEmpty();
                         return false;
                     }
 
                     var cache = whole_data[key].val;
                     var isWYSIWYG = whole_data[key].type == 'WYSIWYG';
 
-                    showPreview(isWYSIWYG, cache);
+                    me.showPreview(isWYSIWYG, cache);
                 });
             });
         }));
     }
 };
-
 
 window.onload = panel.init;
