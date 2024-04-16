cdf1fab30a2db7af5e16c28d8081339819d0b026
cerenkov <162771207+crnkv@users.noreply.github.com>
2024-04-16 11:27:22 +0800

    get browser.idle API

--- common.js
+++ common.js
@@ -1,4 +1,4 @@
-var { storage, runtime, browserAction, pageAction, tabs, windows, menus, extension } = browser;
+var { storage, runtime, browserAction, pageAction, tabs, windows, menus, extension, idle } = browser;
 var { local } = storage;
 
 var catchErr = e => console.error(e);
