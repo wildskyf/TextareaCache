cc8186b371fc176792c839d6e316fbe2f84049d0
gholk <gholk@disroot.org>
2025-09-19 22:53:11 +0800

    feat: build chromium beta release in script

--- build-chromium.sh
+++ build-chromium.sh
@@ -10,15 +10,31 @@ done
 ) > chromium/background-script-all.js
 
 node -e "
-var o = $(cat manifest.json)
+var o = JSON.parse(fs.readFileSync('manifest.json', 'utf8'))
 o.background = o.browser_specific_settings.chromium_background
 delete o.browser_specific_settings
 var exl = 'tabs menus'.split(' ')
 o.permissions = o.permissions.filter(x => !exl.includes(x))
-console.log(JSON.stringify(o, null, '  '))
-" > chromium/manifest.json
+
+fs.writeFileSync(
+  'chromium/manifest.json', JSON.stringify(o, null, '  '), 'utf8')
+
+o.name += ' BETA'
+o.description = 'THIS EXTENSION IS FOR BETA TESTING\n\n' + o.description
+
+fs.writeFileSync(
+  'chromium/manifest-test.json', JSON.stringify(o, null, '  '), 'utf8')
+"
 
 ln -sr browser-polyfill.min.js* common.js helper.js \
    content-script.js service-worker.js vendor \
    options view css fonts icons _locales \
    chromium
+
+(
+    cd chromium
+    zip -r ../chromium.zip * -x manifest-test.json
+
+    mv manifest-test.json manifest.json
+    zip -r ../chromium-test.zip *
+)
